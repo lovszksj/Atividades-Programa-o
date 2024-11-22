0358d116e97f5015ce089c605d3037afeb368e0e
lovszksj <jullyana.azevedo@escolar.ifrn.edu.br>
2024-11-22 15:16:24 -0300

    Update Q1.py

--- AvaliativaComandoCondicional/Q1.py
+++ AvaliativaComandoCondicional/Q1.py
@@ -1,11 +1,11 @@
 # código feito por Jullyana Pessoa e João Victor Gomes
 
 try:
-    # Aqui pedi para que o usuário digitasse os valores de seu peso e de sua altura
+    # Aqui pedimos para que o usuário digitasse os valores de seu peso e de sua altura
     peso = float(input("Digite o seu peso em kg: "))
     altura = float(input("Digite a sua altura em cm: "))
 
-    # Como a questão pede em "m", peguei o valor em cm, digitado pelo usuário, e dividi por 100 para transformá-lo em m
+    # Como a questão pede em "m", peguamos o valor em cm, digitado pelo usuário, e dividimos por 100 para transformá-lo em m
     altura = altura / 100
 
     # Aqui pegamos o valor do peso e dividimos ele pela altura ao quadrado e mostramos no print qual seria o IMC
@@ -13,7 +13,7 @@ try:
     print("O seu IMC é: ", imc)
 
     # Como solicitado na questão, além de declararmos o valor do IMC também pede para falarmos do nível em qual o usuário está 
-    # Comecei pelo o de menor valor e fui aumentando até chegar ao 40 como pedido na questão, usando os comandos if de inicial e continuando com o elif
+    # Começamos pelo o de menor valor e fomos aumentando até chegar ao 40 como pedido na questão, usando os comandos if de inicial e continuando com o elif
     if imc <= 18.5:
         print("Seu IMC está abaixo do normal")
     
@@ -32,6 +32,6 @@ try:
     elif 40.0 <= imc:
         print ("Seu IMC está na obesidade grau III")
 
-# utilizei os comandos try e except para o programa não travar, caso o usuário digite um valor inválido ou até mesmo letra
+# utilizamos os comandos try e except para o programa não travar, caso o usuário digite um valor inválido ou até mesmo letra
 except ValueError: 
     print("Digite um valor válido")
