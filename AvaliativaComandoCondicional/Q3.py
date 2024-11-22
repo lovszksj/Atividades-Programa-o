4a0716719371d80dbb0073888ea40533f9ad24fb
lovszksj <jullyana.azevedo@escolar.ifrn.edu.br>
2024-11-22 15:26:46 -0300

    Update Q3.py

--- AvaliativaComandoCondicional/Q3.py
+++ AvaliativaComandoCondicional/Q3.py
@@ -1,11 +1,16 @@
 # código feito por Jullyana Pessoa e João Victor Gomes
+# como liberado na questão, importamos a função random para sempre gerar um número aleatório
 import random
 try:
+    # declaramos a variável com a importação do random.randint, que sempre irá sortear um número de 1 a 100
     numero = random.randint(1, 100)
+    # já essas declaramos para utilizar na exibição dos intervalos 
     nmaximo = 100
     nminimo = 1
 
+    # aqui iniciamos o jogo de tentativa para acertar o número
     resposta1= int(input("Digite qual número você acha que foi sorteado: "))
+    # caso acerte, irá aparecer a mensagem de acerto
     if resposta1 == numero:
         print("Parabéns! Você acertou")
     else:
@@ -13,8 +18,10 @@ try:
             nminimo = resposta1 + 1
         else:
             nmaximo = resposta1 - 1
+            # caso erre, irá aparecer a estimativa/intervalo dos números no qual o usuário digitou
         print("Tente entre", nminimo, "e", nmaximo)
 
+        # e aparecerá novamente a pergunta de "chute" de qual número foi sorteado, assim repetindo o mesmo processo explicando anteriormente
         resposta2 = int(input("Não foi dessa vez! Digite outro número: "))
         if resposta2 == numero:
             print("Parabéns! Você acertou")
@@ -34,14 +41,15 @@ try:
                 else:
                     nmaximo = resposta3 - 1
                 print("Tente entre", nminimo, "e", nmaximo)
-            
+
+                # como solicitado na questão, serão apenas 4 tentativas, dessa vez caso a pessoa erre novamente
                 resposta4 = int(input("Não foi dessa vez! Digite outro número: "))
                 if resposta4 == numero:
                     print("Parabéns! Você acertou")
                 else:
-                    print("O número sorteado foi:", numero)
+                    # irá aparecer qual era o número e falando para o usuário tentar novamente
                     print("Infelizmente não foi dessa vez! Tente novamente uma outra hora")
-
+                    print("O número sorteado foi:", numero)
 except  ValueError:
     print("Digite um valor válido")
 
